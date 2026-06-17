df0be42749082e0d434181dd404e3e0441353a59
aurakresa <aurakresa12@gmail.com>
2026-06-17 16:03:18 +0700

    Fix router import error and apply proper logging backend

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const verifyFirebaseToken = require('../middlewares/auth');
-const { updateUsername, updateProfilePicture } = require('../controllers/userController');
 
-// Terapkan middleware satpam ke endpoint ini
+// Import semua fungsi dari controller, termasuk getUserLogs
+const { updateUsername, updateProfilePicture, getUserLogs } = require('../controllers/userController');
+
+// Daftar rute
 router.put('/username', verifyFirebaseToken, updateUsername);
 router.put('/profile-picture', verifyFirebaseToken, updateProfilePicture);
 router.get('/logs', verifyFirebaseToken, getUserLogs);
